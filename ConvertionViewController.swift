440cc01295d537bea2c6706588136b04d7175c21
Raditia Madya <gustiraditia@gmail.com>
2017-05-26 00:37:09 +0700

    initial upload

---
+++ ConvertionViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ConvertionViewController.swift
+//  TempConverter
+//
+//  Created by Raditia Madya on 5/25/17.
+//  Copyright © 2017 Universitas Gadjah Mada. All rights reserved.
+//
+
+import UIKit
+
+class ConvertionViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var celciusLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+            updateCelciusLabel()
+        }
+    }
+    
+    var celciusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        }
+        else {
+            return nil
+        }
+    }
+    
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        
+        return nf
+    }()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateCelciusLabel()
+    }
+    
+    @IBAction func valueChanged(_ sender: UITextField) {
+        if let text = sender.text, let value = Double(text) {
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+        }
+        else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    @IBAction func dismissKeyboard (_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
+    }
+    
+    func updateCelciusLabel() {
+        if let celciusValue = celciusValue {
+            celciusLabel.text = numberFormatter.string(from: NSNumber(value: celciusValue.value))
+        }
+        else {
+            celciusLabel.text = "???"
+        }
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        print("Current text: \(string)")
+        print("Replacement text: \(string)")
+        
+        return true
+    }
+    
+    
+    
+}
